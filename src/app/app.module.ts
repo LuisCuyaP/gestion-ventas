a6802fe5d1879ab8c2192742914c13483920b200
Luis <luiscuya@gmail.com>
2023-03-04 19:50:03 -0500

    primer commit - agregar proveedor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {Routes, RouterModule} from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { ListaProveedorComponent } from './componentes/mantenimiento/proveedor/lista-proveedor/lista-proveedor.component';
+
+const appRoutes: Routes = [
+  {path: '', component: AppComponent},
+  {path: 'lista-proveedor', component: ListaProveedorComponent},
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ListaProveedorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
